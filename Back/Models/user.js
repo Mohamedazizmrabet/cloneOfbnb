2d4ddde79f853022c4d2f9c90cc37041c0a463f5
Mohamed Mrabet <81630459+Mohamedazizmrabet@users.noreply.github.com>
2024-10-11 12:01:56 +0100

    fixed db

--- Back/Models/user.js
+++ Back/Models/user.js
@@ -1,64 +1,62 @@
-const {DataTypes,Sequelize} =require('sequelize')
-const sequelize=require('../database/index.js')
+const { DataTypes, Sequelize } = require("sequelize");
+const sequelize = require("../database/index.js");
 
+console.log("hi");
 
-const User = sequelize.define('user', {
+const User = sequelize.define(
+  "user",
+  {
     iduser: {
       type: DataTypes.INTEGER,
       autoIncrement: true,
       primaryKey: true,
     },
     userName: {
-        type: DataTypes.STRING(45),
-        allowNull: true,
-      },
+      type: DataTypes.STRING(45),
+      allowNull: true,
+    },
     userEmail: {
-        type: DataTypes.STRING(45),
-        allowNull: true,
-      },
+      type: DataTypes.STRING(45),
+      allowNull: true,
+    },
     userPassword: {
       type: DataTypes.STRING(255),
       allowNull: true,
     },
     userImage: {
-        type : DataTypes.TEXT,
-        allowNull : true,
+      type: DataTypes.TEXT,
+      allowNull: true,
     },
-    userBlocked : {
-        type: DataTypes.TINYINT,
-        allowNull : true,
+    userBlocked: {
+      type: DataTypes.TINYINT,
+      allowNull: true,
     },
-    userLatitude : {
-        type : DataTypes.STRING(45),
-        allowNull : true,
+    userLatitude: {
+      type: DataTypes.STRING(45),
+      allowNull: true,
     },
-    userLongitude : {
-        type : DataTypes.STRING(45),
-        allowNull : true,
+    userLongitude: {
+      type: DataTypes.STRING(45),
+      allowNull: true,
     },
     adress: {
-      type :DataTypes.STRING(45),
-      allowNull: true
-
+      type: DataTypes.STRING(45),
+      allowNull: true,
     },
     city: {
-      type :DataTypes.STRING(45),
-      allowNull: true
-
+      type: DataTypes.STRING(45),
+      allowNull: true,
     },
     state: {
-      type :DataTypes.STRING(45),
-      allowNull: true
-
+      type: DataTypes.STRING(45),
+      allowNull: true,
     },
     contactNumber: {
-      type :DataTypes.NUMBER(8),
-      allowNull: true
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+  },
+  { tableName: "user" }
+);
 
-    }
-  
-  },{tableName:'user'});
-  
-   
-   
-  module.exports= User
+module.exports = User;
--- Back/database/index.js
+++ Back/database/index.js
@@ -1,28 +1,31 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
- const sequelize = new Sequelize(
-
-  "thesisrbk",
-  "root",
-  "farahkh",
+const sequelize = new Sequelize(
+  "clonerbnb",
+  "mrabet",
+  "clavier123",
 
   {
     host: "localhost",
     dialect: "mysql",
-    define: {     timestamps: false  },
+    define: { timestamps: false },
   }
 );
 
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Team7 connected");
+  })
+  .catch((err) => console.log("Unable to connect :", err));
 
-sequelize.authenticate().then(()=>{console.log('Team7 connected')})
-.catch(err => console.log('Unable to connect :', err))
+sequelize
+  .sync()
+  .then(() => {
+    console.log(" Tables synchronized.");
+  })
+  .catch((error) => {
+    console.error("Error in synchronization :", error);
+  });
 
-// sequelize.sync()
-//   .then(() => {
-//     console.log(' Tables synchronized.');
-//   })
-//   .catch((error) => {
-//     console.error('Error in synchronization :', error);
-//   });
-
-  module.exports = sequelize;
+module.exports = sequelize;
